790f21b57031cef8f6f3b23a3e2efaecc5cdc143
jamesandere <jamesandere56@gmail.com>
2019-02-16 13:57:36 +0300

    Declare variables

--- Js/script.js
+++ Js/script.js
@@ -2,3 +2,6 @@ function track(){
 var sideA=parseInt(document.getElementById('A').value);
 var sideB=parseInt(document.getElementById('B').value);
 var sideC=parseInt(document.getElementById('C').value);
+if(sideA==isNaN || sideB==isNaN || sideC==isNaN){
+  document.getElementById("output").innerHTML ="peleka ufala kando";
+}
