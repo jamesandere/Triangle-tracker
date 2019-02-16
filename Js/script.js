7127d11013e1f41eabad8d29e9f510bb6ca155f3
jamesandere <jamesandere56@gmail.com>
2019-02-16 14:02:12 +0300

    Use control flows

--- Js/script.js
+++ Js/script.js
@@ -5,3 +5,23 @@ var sideC=parseInt(document.getElementById('C').value);
 if(sideA==isNaN || sideB==isNaN || sideC==isNaN){
   document.getElementById("output").innerHTML ="peleka ufala kando";
 }
+else{
+  if(sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA >sideB){
+    if(sideA==sideB && sideB==sideC && sideC==sideA){
+    document.getElementById("output").innerHTML = "This is an equilateral triangle!";
+    document.getElementById("img").src= "https://www.pngkey.com/png/full/423-4237927_drawing-of-equilateral-triangle.png"
+      }
+    else if (sideA!==sideB && sideB!==sideC && sideC!== sideA){
+    document.getElementById("output").innerHTML ="This is a scalene triangle!";
+    document.getElementById("img").innerHTML ="";
+    }
+    else {
+    document.getElementById("output").innerHTML ="This is an isosceles triangle";
+    document.getElementById("img").innerHTML ="";
+    }
+}
+else{
+  document.getElementById("output").innerHTML ="This is not a triangle";
+  document.getElementById("img").innerHTML ="";
+  }
+}
