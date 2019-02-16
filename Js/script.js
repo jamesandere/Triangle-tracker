0f79cf6899eeba6cef8abe9f3483ae5e478b4ee2
jamesandere <jamesandere56@gmail.com>
2019-02-16 13:56:12 +0300

    Create a function

---
+++ Js/script.js
@@ -0,0 +1,4 @@
+function track(){
+var sideA=parseInt(document.getElementById('A').value);
+var sideB=parseInt(document.getElementById('B').value);
+var sideC=parseInt(document.getElementById('C').value);
