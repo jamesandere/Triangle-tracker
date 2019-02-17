80468c5c8bd77f5d75500c4f8f51815e179b50d3
jamesandere <jamesandere56@gmail.com>
2019-02-17 16:58:07 +0300

    Change HTML contents

--- Js/script.js
+++ Js/script.js
@@ -4,7 +4,7 @@ var sideB=parseInt(document.getElementById('B').value);
 var sideC=parseInt(document.getElementById('C').value);
 
 if(isNaN(sideA) || isNaN(sideB) || isNaN(sideC)){
-
+  document.getElementById("output").innerHTML= "Not a number!";document.getElementById("output").innerHTML = "This is an equilateral triangle!";
 }
 else{
   if(sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA >sideB){
@@ -12,14 +12,14 @@ else{
 
       }
     else if (sideA!==sideB && sideB!==sideC && sideC!== sideA){
-
+       document.getElementById("output").innerHTML ="This is a scalene triangle!";
     }
     else {
-
+       document.getElementById("output").innerHTML ="This is an isosceles triangle!";
         }
     }
     else{
-      
+        document.getElementById("output").innerHTML ="This is not a triangle!";
       }
     }
     }
