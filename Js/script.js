6c55a51fc1b41c6e27aef2ed0eaea2d2706ae454
jamesandere <jamesandere56@gmail.com>
2019-02-17 16:52:04 +0300

    Use control flows with logical operators

--- Js/script.js
+++ Js/script.js
@@ -2,4 +2,24 @@ function track(){
 var sideA=parseInt(document.getElementById('A').value);
 var sideB=parseInt(document.getElementById('B').value);
 var sideC=parseInt(document.getElementById('C').value);
+
+if(isNaN(sideA) || isNaN(sideB) || isNaN(sideC)){
+
 }
+else{
+  if(sideA + sideB > sideC && sideB + sideC > sideA && sideC + sideA >sideB){
+    if(sideA==sideB && sideB==sideC && sideC==sideA){
+
+      }
+    else if (sideA!==sideB && sideB!==sideC && sideC!== sideA){
+
+    }
+    else {
+
+        }
+    }
+    else{
+      
+      }
+    }
+    }
